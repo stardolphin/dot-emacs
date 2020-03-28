7e92e92b1c014c994007f733a207a8a29dd46609
stardolphin <dolphinlink@gmail.com>
2020-03-28 14:34:30 -0600

    Recent changes

---
+++ elpa/archive-downloader-1.1/archive-downloader-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "archive-downloader" "1.1" "Download files from archive.org" 'nil)
--- init.el
+++ init.el
@@ -142,10 +142,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-repl-display-help-banner nil)
+ '(cider-repl-history-file "~/.emacs.d/cider-history")
+ '(cider-repl-wrap-history t)
  '(coffee-tab-width 2)
  '(package-selected-packages
    (quote
-    (magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell))))
+    (archive-downloader magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
